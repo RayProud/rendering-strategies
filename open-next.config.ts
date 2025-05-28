612e73d93e09716dbe590ad7529c9d394fa32f1f
Roman Prudnikov <stillerr@yandex.ru>
2025-05-28 13:51:31 +0200

    Migrate to OpenNextv1

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,28 +1,4 @@
-import cache from '@opennextjs/cloudflare/kvCache';
+// default open-next.config.ts file created by @opennextjs/cloudflare
+import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
 
-const config = {
-  default: {
-    override: {
-      wrapper: 'cloudflare-node',
-      converter: 'edge',
-      incrementalCache: async () => cache,
-      tagCache: 'dummy',
-      queue: 'dummy',
-    },
-  },
-
-  middleware: {
-    external: true,
-    override: {
-      wrapper: 'cloudflare-edge',
-      converter: 'edge',
-      proxyExternalRequest: 'fetch',
-    },
-  },
-
-  dangerous: {
-    enableCacheInterception: false,
-  },
-};
-
-export default config;
+export default defineCloudflareConfig({});
