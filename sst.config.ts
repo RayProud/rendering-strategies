4ddb65c95c489488d5cbc7b6df30c5ae1df4378c
Sofia Canclini <sofia.canclini@mentimeter.com>
2025-06-03 14:49:33 +0200

    config changes

---
+++ sst.config.ts
@@ -0,0 +1,24 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "aws-nextjs",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+              providers: {
+        aws: {
+          profile: 'sandbox-poweruser-set-mentimeter-dev'
+        }
+      }
+    };
+  },
+  async run() {
+    const bucket = new sst.aws.Bucket("MyBucket", {
+      access: "public"
+    });
+    new sst.aws.Nextjs("MyWeb", {
+      link: [bucket]
+    });
+  }
+});
