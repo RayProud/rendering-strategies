3e16930c215a80d9e12d6f542c5d9d5bb9c4c1bf
Roman Prudnikov <stillerr@yandex.ru>
2025-06-09 15:31:11 +0200

    OpenNext for Cloudflare on Next 14

---
+++ applications/cloudflare-opennext-14/src/app/server-component-with-dynamic-import.tsx
@@ -0,0 +1,21 @@
+import React, { Suspense } from 'react';
+import { promises as fs } from 'fs';
+
+const StateComponent = React.lazy(() => import('./state-component'));
+
+export default async function ServerComponent() {
+  console.log('ServerComponent message');
+  const env = process.env.NODE_ENV;
+
+  const files = await fs.readdir('.');
+
+  return (
+    <div>
+      <p>Server Component: our NODE_ENV is {env}</p>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <StateComponent files={files} />
+      </Suspense>
+    </div>
+  );
+}
---
+++ applications/cloudflare-opennext-14/src/app/server-static/page.tsx
@@ -0,0 +1,18 @@
+export const runtime = 'nodejs';
+
+export default function Server() {
+  console.log('Server Side Static page');
+  return (
+    <div>
+      <h1>
+        Server Side Static page{' '}
+        <time
+          dateTime={new Date().toLocaleTimeString()}
+          suppressHydrationWarning
+        >
+          {new Date().toLocaleTimeString()}
+        </time>
+      </h1>
+    </div>
+  );
+}
