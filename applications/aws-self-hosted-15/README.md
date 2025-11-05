5d9de0999c8a585ec5e2f595b8fbaf938880aab1
Roman Prudnikov <stillerr@yandex.ru>
2025-11-05 16:54:56 +0100

    Reproduce no user-agent header for OpenNext

--- applications/aws-self-hosted-15/README.md
+++
@@ -1,7 +0,0 @@
-## How to run an image
-
-- Go to the root
-- `colima start`
-- `docker build -t menti .` (you can use any tag instead of `menti`)
-- `docker run --publish 3000:3000 menti:latest`
-- go to `localhost:3000`
--- applications/cloudflare-opennext-15/src/next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-import path from 'node:path';
-
-const nextConfig = {
-  output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname, '../../'),
-};
-
-export default nextConfig;
